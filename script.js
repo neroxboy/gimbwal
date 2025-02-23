9f5e1cd88d5c0f9cfdb14e877b7db76f903ed109
neroxboys <103934814+neroxkira@users.noreply.github.com>
2025-02-24 00:14:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ function getFarPosition(containerRect, windowWidth, windowHeight, buttonWidth, b
 
 yesBtn.addEventListener('click', () => {
     title.innerHTML = "HORE!!, yaudaa mau eskrimm apaaa";
-    document.querySelector('img').remove(); 
+    document.querySelector('img').src = "https://media.tenor.com/gUiu1zyxfzYAAAAi/good-night-kiss-kiss.gif";
     noBtn.style.display = 'none';
     yesBtn.style.display = 'none';
     bgMusic.play();
